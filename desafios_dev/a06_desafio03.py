efd71ae0423ee92f881228560ea88d65a40e5360
L <13luizhenrique@gmail.com>
2024-07-01 18:19:32 +0000

    State tuples with description

---
+++ desafios_dev/a06_desafio03.py
@@ -0,0 +1,105 @@
+import tkinter as tk
+
+TuplaUFS = (
+    "AC",
+    "AL",
+    "AP",
+    "AM",
+    "BA",
+    "CE",
+    "DF",
+    "ES",
+    "GO",
+    "MA",
+    "MT",
+    "MS",
+    "MG",
+    "PA",
+    "PB",
+    "PR",
+    "PE",
+    "PI",
+    "RJ",
+    "RN",
+    "RS",
+    "RO",
+    "RR",
+    "SC",
+    "SP",
+    "SE",
+    "TO",
+)
+TuplasDescricao = (
+    "Acre",
+    "Alagoas",
+    "Amapá",
+    "Amazonas",
+    "Bahia",
+    "Ceará",
+    "Distrito Federal",
+    "Espírito Santo",
+    "Goiás",
+    "Maranhão",
+    "Mato Grosso",
+    "Mato Grosso do Sul",
+    "Minas Gerais",
+    "Pará",
+    "Paraíba",
+    "Paraná",
+    "Pernambuco",
+    "Piauí",
+    "Rio de Janeiro",
+    "Rio Grande do Norte",
+    "Rio Grande do Sul",
+    "Rondônia",
+    "Roraima",
+    "Santa Catarina",
+    "São Paulo",
+    "Sergipe",
+    "Tocantins",
+)
+ListaUFS = []
+
+janela = tk.Tk()
+janela.title("Estados Brasileiros")
+janela.resizable(False, False)
+janela.attributes("-toolwindow", True)
+janela.attributes("-topmost", True)
+
+label = tk.Label(janela, text="Adicionar estado")
+label.pack(padx=10, pady=5)
+entry0 = tk.Entry(janela)
+entry0.pack(padx=10, pady=5)
+label2 = tk.Label(janela, text="Estados adicionados")
+label2.pack(padx=10, pady=5)
+
+
+def adicionar():
+    if entry0.get() in ListaUFS:
+        return label.config(text="Estado já adicionado")
+    elif entry0.get() not in TuplaUFS:
+        return label.config(text="Estado inválido")
+    ListaUFS.append(entry0.get())
+    label3 = tk.Label(
+        janela,
+        text="[X] "
+        + entry0.get()
+        + " - "
+        + TuplasDescricao[TuplaUFS.index(entry0.get())],
+        justify="left",
+        cursor="hand2",
+    )
+    label3.pack(padx=10, pady=0)
+    entry0.delete(0, tk.END)
+
+    def deletethis():
+        currentUf = label3.cget("text")[4:6]
+        ListaUFS.remove(currentUf)
+        label3.destroy()
+
+    label3.bind("<Button-1>", lambda x: deletethis())
+
+
+entry0.bind("<Return>", lambda x: adicionar())
+
+janela.mainloop()
