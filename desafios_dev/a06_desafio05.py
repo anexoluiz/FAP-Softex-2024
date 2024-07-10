c6e93bea6c02ace2bfb868b2e612daa73a3540b1
Luiz Henrique <13luizhenrique@gmail.com>
2024-07-09 22:31:08 -0300

    fix editing and delete,  semantic and layout

--- desafios_dev/a06_desafio05.py
+++ desafios_dev/a06_desafio05.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-tarefas = []
+tarefasLista = []
 
 tarefaEmEdicao = None
 tarefaEmEdicaoIndex = None
@@ -11,52 +11,62 @@ janela.resizable(False, False)
 janela.attributes("-toolwindow", True)
 janela.attributes("-topmost", True)
 
-label = tk.Label(janela, text="Adicionar tarefa")
-entry0 = tk.Entry(janela)
-label.grid(row=0, column=0, padx=5, pady=5)
-entry0.grid(row=0, column=1, padx=5, pady=5)
+labelAdicionar = tk.Label(janela, text="Adicionar tarefa")
+inputAdicionar = tk.Entry(janela)
+labelAdicionar.grid(row=0, column=0, padx=5, pady=5)
+inputAdicionar.grid(row=0, column=1, columnspan=8, padx=5, pady=5)
+
 
 def adicionar():
-    tarefa = entry0.get()
-    if (entry0.get() == ""):
+    tarefaAtual = inputAdicionar.get()
+    if inputAdicionar.get() == "" or tarefasLista.count(tarefaAtual) > 0:
         return None
-    if tarefa:
+    if tarefaAtual:
         global tarefaEmEdicao
         global tarefaEmEdicaoIndex
-        if (tarefaEmEdicaoIndex != None):
-            tarefas[tarefaEmEdicaoIndex] = tarefa
-            tarefaEmEdicao.config(text=str(tarefaEmEdicaoIndex+1)+". "+tarefa)
-            entry0.delete(0,tk.END)
+        if tarefaEmEdicaoIndex != None:
+            tarefasLista[tarefaEmEdicaoIndex] = tarefaAtual
+            tarefaEmEdicao.config(text="- " + tarefaAtual)
+            inputAdicionar.delete(0, tk.END)
             janela.update()
             tarefaEmEdicao = None
             tarefaEmEdicaoIndex = None
             return None
-        tarefas.append(tarefa)
-        index = len(tarefas) - 1
-        entry0.delete(0, tk.END)
-        label0 = tk.Label(janela, text=str(len(tarefas)) + ". " + tarefa)
-        button0 = tk.Button(janela, text="✏️", cursor="hand2")
-        button1 = tk.Button(janela, text="❌", cursor="hand2")
-        label0.grid(row=index + 1, column=0, padx=5, pady=5)
-        button0.grid(row=index + 1, column=1, padx=5, pady=5)
-        button1.grid(row=index + 1, column=2, padx=5, pady=5)
-        def excluir(index):
-            tarefas.pop(index)
-            label0.destroy()
-            button0.destroy()
-            button1.destroy()
-        def editar(index):
+        tarefasLista.append(tarefaAtual)
+        index = len(tarefasLista) - 1
+        inputAdicionar.delete(0, tk.END)  # Limpa o campo de texto
+        novaTarefa = tk.Label(janela, text="- " + tarefaAtual, justify=tk.LEFT, wraplength=200)
+        botaoEditar = tk.Button(
+            janela, text="✏️", cursor="hand2", font=("Segoe UI Emoji", 8)
+        )
+        botaoExcluir = tk.Button(
+            janela, text="❌", cursor="hand2", font=("Segoe UI Emoji", 8)
+        )
+        novaTarefa.grid(row=index + 1, column=0, columnspan=7, padx=5, pady=5, sticky="w")
+        botaoEditar.grid(row=index + 1, column=7, padx=2, pady=5)
+        botaoExcluir.grid(row=index + 1, column=8, padx=2, pady=5)
+
+        def excluir():
+            novoIndice = tarefasLista.index(novaTarefa.cget("text")[2:])
+            tarefasLista.pop(novoIndice)
+            novaTarefa.destroy()
+            botaoEditar.destroy()
+            botaoExcluir.destroy()
+
+        def editar():
             global tarefaEmEdicao
             global tarefaEmEdicaoIndex
-            tarefaEmEdicao = label0
-            tarefaEmEdicaoIndex = index
-            entry0.delete(0, tk.END)
-            entry0.insert(0, tarefas[index])
-        button0.bind("<Button-1>", lambda e: editar(index))
-        button1.bind("<Button-1>", lambda e: excluir(index))
-    else:
-        entry0.focus()
+            tarefaEmEdicao = novaTarefa
+            tarefaEmEdicaoIndex = tarefasLista.index(novaTarefa.cget("text")[2:])
+            inputAdicionar.delete(0, tk.END)
+            inputAdicionar.insert(0, tarefasLista[tarefaEmEdicaoIndex])
 
-entry0.bind("<Return>", lambda e: adicionar())
-    
+        botaoEditar.bind("<Button-1>", lambda e: editar())
+        botaoExcluir.bind("<Button-1>", lambda e: excluir())
+    else:
+        inputAdicionar.focus()
+
+
+inputAdicionar.bind("<Return>", lambda e: adicionar())
+
 janela.mainloop()
